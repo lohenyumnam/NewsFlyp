6c7a38ce02205e7c7310c602ff0797d8225f0f8e
LohenYumnam <17508663+lohenyumnam@users.noreply.github.com>
2018-09-05 10:56:42 +0530

    Initial Commit

---
+++ NewsFlyp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsFlyp
+//
+//  Created by Lohen Yumnam on 05/09/18.
+//  Copyright © 2018 Lohen Yumnam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
